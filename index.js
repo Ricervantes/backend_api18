43f5ddb2ba45eadbf786a0b3fc56a93844329df1
Ricervantes <racervantes.dwfsc9@ucamp.utel.edu.mx>
2022-11-15 09:17:00 -0600

    commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //usamos el frameworks express para a API rest
-const express=require('express');
+const express = require('express');
 //usaremos cors para permitir conexiones  entre distintos dominios localhost
-const cors= require('cors');
+const cors = require('cors');
+require('dotenv').config();
 //ponemos express en la variable server
 const server = express();
 //usamos el middleware de cors de express de cor que importamos previamente
@@ -9,11 +10,11 @@ server.use(cors());
 //usamos el middleware de express.json para poder leer los datos que nos envian en el body
 server.use(express.json());
 //ponemos el puerto en una constante
-const PORT=process.env.PORT;
+const PORT = process.env.PORT;
 //creamos un array de objetos para simular una base de datos
-let platillos =[
-    {nombre:'chocolates', precio:50},
-    {nombre:"mole", precio:100},
+let platillos = [
+    { nombre: 'chocolates', precio: 50 },
+    { nombre: "mole", precio: 100 },
 ];
 
 //verbos http
@@ -22,70 +23,71 @@ let platillos =[
 //put actualizar datos
 //delete eliminar datos
 //creando endpoint principal
-server.get("/",(request,response)=>{
+server.get("/", (request, response) => {
     response.send("API v1.01");
 });
-server.get("/platillos",(request,response)=>{
-   // response.send(platillos);
+server.get("/platillos", (request, response) => {
+    // response.send(platillos);
     response.json(
-    {
-        //data:platillos,
-        data:platillos.map((platillo, index)=>{
-            return {index,...platillo};
-        }),
-        count:platillos.length,
-        mensaje:"platillos obtenidos correctamente"
-    }
+        {
+            //data:platillos,
+            data: platillos.map((platillo, index) => {
+                return { index, ...platillo };
+            }),
+            count: platillos.length,
+            mensaje: "platillos obtenidos correctamente"
+        }
     );
 });
-    // creando el endpoint para agregar un platillo
-server.post("/platillos",(request,response)=>{
-   // response.send(platillos);
-   //como lo agrego al array ?
-   const platillo=request.body;
-   if(platillo.nombre){
-    return response.status(400).json({mensaje:"el pletillo necesita un nombre"});
-   }
-   platillos.push(platillo);
-    response.json(
-    {
-        data:platillo,
-       mensaje:"entro a la funcion de agregar platillos"
+// creando el endpoint para agregar un platillo
+server.post("/platillos", (request, response) => {
+    // response.send(platillos);
+    //como lo agrego al array ?
+    const platillo = request.body;
+    if (!platillo.nombre || !platillo.precio) {
+        return response.status(400).json({ mensaje: "el platillo necesita un nombre y un precio" });
     }
-    );
-
-    //una funcion de flecha que accede a un req o res es una middleware
-});
-server.put("/platillos:index",(request,response)=>{
-    const {index}=request.params;
-    const platillo=request.body;
-    platillos[index]=platillo;
-   // response.send(platillos);
-   //como lo agrego al array ?
+    platillos.push(platillo);
     response.json(
-    {
-        data:platillo,
-       mensaje:"entro a la funcion de agregar platillos"
-    }
+        {
+            data: platillo,
+            mensaje: "entro a la funcion de agregar platillos"
+        }
     );
 
     //una funcion de flecha que accede a un req o res es una middleware
 });
-server.delete("/platillos:index",(request,response)=>{
-    const {index}=request.params;
-     // sacaremos el  indice el parametro de la url
- platillos.splice(index,1);
-   //eliminaremos el elemento del array
+server.put("/platillos/:index", (request, response) => {
+    const { index } = request.params;
+    const platillo = request.body;
+    platillos[index] = platillo;
+    // response.send(platillos);
+    //como lo agrego al array ?
     response.json(
-    {
-        data:platillo,
-       mensaje:"entro a la funcion de agregar platillos"
-    }
+        {
+            data: platillo,
+            mensaje: "entro a la funcion de agregar platillos"
+        }
+    );
+
+    //una funcion de flecha que accede a un req o res es una middleware
+});
+//agregar el /al final del endpoint
+server.delete("/platillos/:index", (request, response) => {
+    const { index } = request.params;
+    // sacaremos el  indice el parametro de la url
+    platillos.splice(index, 1);
+    //eliminaremos el elemento del array
+    response.json(
+        {
+            mensaje: "platillo eliminado correctamente"
+        }
     );
 
     //una funcion de flecha que accede a un req o res es una middleware
 });
 //iniciamos el servidor de express
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log("se elimino correctamente");
 });
+
